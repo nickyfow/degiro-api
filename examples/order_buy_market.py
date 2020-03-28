aa2617329211ce7187a79a474723d509b45fabc0
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-03-28 23:43:26 +0000

    Comments

--- examples/order_buy_market.py
+++ examples/order_buy_market.py
@@ -1,12 +1,19 @@
 # Issue a market order
+import time
 from degiro import DeGiro
 from degiro.utils import pretty_json
 from degiro.order import Order
 from datetime import datetime
-
 from examples.config import DEGIRO_USERNAME, DEGIRO_PASSWORD
 
 def main():
+  print('This will issue a market order and then cancel it.')
+  print('------')
+  time.sleep(2)
+  print('See the broker page on the left.')
+  print('------')
+  time.sleep(2)
+
   print('Init session')
   degiro = DeGiro()
   degiro.login(DEGIRO_USERNAME, DEGIRO_PASSWORD)
